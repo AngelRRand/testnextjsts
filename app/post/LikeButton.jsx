ad21fe6bfe46f0c0ffb54d4facaea3dd12f4fe5a
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-10 20:11:57 -0300

    asdasda

--- app/post/LikeButton.jsx
+++ app/post/LikeButton.jsx
@@ -6,7 +6,7 @@ export const LikeButton = ({id}) => {
     const [liked, setLiked] = useState(false)
     console.log('a')
     return (
-        <button onClick={setLiked(!liked)}>
+        <button onClick={()=>setLiked(!liked)}>
             {liked ? 'Ta lendoo' : 'Meh'}
         </button>
     )

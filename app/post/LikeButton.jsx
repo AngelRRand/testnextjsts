262c5127934ef4e51621ba60f267d08b15b853bb
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-10 20:12:42 -0300

    vamo messi

--- app/post/LikeButton.jsx
+++ app/post/LikeButton.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 
 export const LikeButton = ({id}) => {
     const [liked, setLiked] = useState(false)
-    console.log('a')
     return (
         <button onClick={()=>setLiked(!liked)}>
             {liked ? 'Ta lendoo' : 'Meh'}
